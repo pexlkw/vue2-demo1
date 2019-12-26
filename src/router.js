28dd7827c1e7b99ed2ac61362195bdb581143ab0
sarah.shih <sarah.shih@g-softtech.com>
2019-12-26 17:55:04 +0800

    fix: 登入同步問題處理

--- src/router.js
+++ src/router.js
@@ -49,27 +49,29 @@ const router = new Router({
 /**
  * 判斷 token 是否有過期,
  */
-
-function getLoginEvent () {
-  if (!store.getters.isLogin && store.getters.token) { // 未登入但 localstorage 有 token時
+const getLoginEvent = new Promise((resolve, reject) => {
+  console.log('Promise 開始')
+  if (!store.getters.isLogin && store.getters.token) {
     store.dispatch('UserInfo').then(() => {
-      return store.getters.isLogin;
+      resolve(store.getters.isLogin);
     });
-  } else { // 已登入 or 未登入
-    return store.getters.isLogin
+  } else {
+    // 回傳失敗
+    reject(store.getters.isLogin);
   }
-}
+});
 
 router.beforeEach((to, from, next) => {
-  // console.log('to=', to.fullPath, '| from=', from.fullPath, to.path);
   const hasRequiresAuth = to.matched.some(record => record.meta.requiresAuth);
-  const hasLogin = getLoginEvent();
+  const hasLogin = store.getters.isLogin;
   if (to.path === '/login' && hasLogin) { // 登入頁且已登入時
     console.log('login', hasRequiresAuth, hasLogin);
     next();
   } else if (hasRequiresAuth && !hasLogin) { // 須登入而未登入時
-    store.dispatch('UserInfo').then(() => {
-      console.log('nnn', hasRequiresAuth, hasLogin);
+    console.log('testtt');
+    getLoginEvent.then((data) => {
+      next();
+    }).catch(() => {
       next({
         path: '/login',
         query: { redirect: to.fullPath }
